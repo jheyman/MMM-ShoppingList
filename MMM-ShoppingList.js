4f2935c889e1f8fa265874dede62e2875a0bcd64
jheyman <bidsomail@gmail.com>
2020-04-09 22:24:06 +0200

    implemented retrieval of remote list

--- MMM-ShoppingList.js
+++ MMM-ShoppingList.js
@@ -2,88 +2,96 @@
 /* global Module */
 
 /* Magic Mirror
- * Module: MMM-ShoppingList
- *
- * By jheyman
- * MIT Licensed.
- */
+* Module: MMM-ShoppingList
+*
+* By jheyman
+* MIT Licensed.
+*/
 
 Module.register("MMM-ShoppingList", {
-	defaults: {
-		xxx: 10,
-
-	},
-
-	notificationReceived: function(notification, payload, sender) {
-		if (notification === "ALL_MODULES_STARTED") {
-			this.sendSocketNotification("ALL_MODULES_STARTED");
-		}
-	},
-
-	// Override socket notification handler.
-	socketNotificationReceived: function(notification, payload) {
-		if (notification === "XXXXX") {
-
-		} 
-	},
-
-	start: function() {
-
-		Log.info("Starting module: " + this.name + this.identifier);
-	},
-
-	getStyles: function() {
-		return ["shoppinglist.css"];
-	},
-
-	getDom: function() {
-
+    defaults: {
+        url: "http://192.168.0.13:8081/shoppinglist.php",
+        
+    },
+    
+    notificationReceived: function(notification, payload, sender) {
+        console.log("MMM-ShoppingList received " + notification);
+        if (notification === "ALL_MODULES_STARTED") {
+            this.sendSocketNotification("REFRESH_SHOPPINGLIST", this.config.url);
+        } else if (notification === "REFRESH_SHOPPINGLIST") {
+            this.sendSocketNotification("REFRESH_SHOPPINGLIST", this.config.url);
+        }
+    },
+    
+    // Override socket notification handler.
+    socketNotificationReceived: function(notification, payload) {
+        console.log("MMM-ShoppingList (socket)received " + notification);
+        if (notification === "MMM-ShoppingList_SHOPPINGLIST_ITEMS") {
+            this.shoppingListItems = payload.items;
+            this.updateDom();
+        } 
+    },
+    
+    start: function() {
+        this.shoppingListItems = [];
+        Log.info("Starting module: " + this.name + this.identifier);
+    },
+    
+    getStyles: function() {
+        return ["shoppinglist.css"];
+    },
+    
+    getDom: function() {
+        
         var container = document.createElement("div");
         container.className = "ShoppingListWrapper";
-            
-            this.shoppingListContainer = document.createElement("div");
-            this.shoppingListContainer.className = "slContainer";
+        
+        this.shoppingListContainer = document.createElement("div");
+        this.shoppingListContainer.className = "slContainer";
+        
 
-                var menubar = document.createElement("div");
-                menubar.id = "shoppingListMenuBar";
-                menubar.style.display = "block";
-             
-                var addItem = document.createElement("button");
-                addItem.className = "addItemButton";
-                addItem.innerText = "+";
-                addItem.setAttribute("name", "addItemButton");
-                addItem.onclick = () => {
-                    this.log("MMM-ShoppingList addItem triggered");
-                    this.sendNotification("SHOPPINGLIST_ADDITEM_SHOWKB", { key: "addShoppingListItem", style: "default"});											
-                }; 
-                menubar.appendChild(addItem);
+        var menubar = document.createElement("div");
+        menubar.id = "shoppingListMenuBar";
+        menubar.style.display = "block";
+        
+        var shoppingListTitle = document.createElement("button");
+        shoppingListTitle.className = "shoppingListTitle";
+        shoppingListTitle.innerText = "COURSES";
 
-                var listContent = document.createElement("div");
-                listContent.id = "shoppingListContent";
-                listContent.style.display = "block";
-
-
-                for (let i = 0; i < 5; i++) {
-                    var listItem = document.createElement("li");
-                    listItem.className = "listItem";
-                    listItem.innerHTML = "listItem"+i;
-                    listContent.appendChild(listItem);
-                }
-
-
-
-
-            this.shoppingListContainer.appendChild(menubar); 
-            this.shoppingListContainer.appendChild(listContent);     
-
+        var addItem = document.createElement("button");
+        addItem.className = "addItemButton";
+        addItem.innerText = "+";
+        addItem.setAttribute("name", "addItemButton");
+        addItem.onclick = () => {
+            this.log("MMM-ShoppingList addItem triggered");
+            this.sendNotification("SHOPPINGLIST_ADDITEM_SHOWKB", { key: "addShoppingListItem", style: "default"});											
+        }; 
+        
+        menubar.appendChild(shoppingListTitle);
+        menubar.appendChild(addItem);
+        
+        var listContent = document.createElement("div");
+        listContent.id = "shoppingListContent";
+        listContent.style.display = "block";
+        
+        for (let i = 0; i < this.shoppingListItems.length; i++) {
+            var listItem = document.createElement("li");
+            listItem.className = "listItem";
+            listItem.innerHTML = this.shoppingListItems[i].item;
+            listContent.appendChild(listItem);
+        }
+        
+        this.shoppingListContainer.appendChild(menubar); 
+        this.shoppingListContainer.appendChild(listContent);     
+        
         container.appendChild(this.shoppingListContainer);
         
         return container;
-
+        
     },
     log: function (msg) {
         if (this.config && this.config.debug) {
-          console.log(this.name + ":", JSON.stringify(msg));
+            console.log(this.name + ":", JSON.stringify(msg));
         }
     }
 });
--- node_helper.js
+++ node_helper.js
@@ -1,23 +1,36 @@
 "use strict";
 
 /* Magic Mirror
- * Module: MMM-ShoppingList
- *
- * By jheyman
- * MIT Licensed.
- */
+* Module: MMM-ShoppingList
+*
+* By jheyman
+* MIT Licensed.
+*/
 
 var NodeHelper = require("node_helper");
+var request = require('request');
 
 module.exports = NodeHelper.create({
-	start: function() {
-
-	},
-
-	// Subclass socketNotificationReceived received.
-	socketNotificationReceived: function(notification, payload) {
-		const self = this;
-		if (notification === "ALL_MODULES_STARTED") {
-		}
-	}
+    start: function() {
+        
+    },
+    
+    getJson: function (url) {
+        var self = this;
+        console.log("MMM-Shoppinglist GET fresh JSON data");
+        request({ url: url, method: 'GET' }, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var json = JSON.parse(body);
+                // Send the json data back with the url to distinguish it on the receiving part
+                self.sendSocketNotification("MMM-ShoppingList_SHOPPINGLIST_ITEMS", json);
+            }
+        });
+    },
+    // Subclass socketNotificationReceived received.
+    socketNotificationReceived: function(notification, url) {
+        const self = this;
+        if (notification === "REFRESH_SHOPPINGLIST") {
+            this.getJson(url);
+        }
+    }
 });
